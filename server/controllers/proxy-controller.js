dd3071192c530f85d99589c2d43ab5e9d694c1e8
Daniel Lochrie <dclochri@gmail.com>
2016-08-27 22:42:24 -0500

    Removed some unneccesary proxy router logic. Fixed a typo.

--- server/controllers/proxy-controller.js
+++ server/controllers/proxy-controller.js
@@ -1,7 +1,10 @@
 /**
  * @fileoverview The Proxy Controller for local and remote requests.
  */
-s/**
+
+
+
+/**
  * Handles client requests for Angular routes.
  * @param {!http.IncomingMessage} req The Node.js server request object.
  * @param {!http.ServerResponse} res The Node.js server response object.
